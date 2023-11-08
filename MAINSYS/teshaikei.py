ad68e6236e6ae6ad5450e4ed967d225f98f20e40
ishikawa <204004@ocsjoho.onmicrosoft.com>
2023-11-08 14:05:34 +0900

    2023/11/08/i

--- MAINSYS/teshaikei.py
+++ MAINSYS/teshaikei.py
@@ -3,15 +3,9 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.graphics import Color, Rectangle
-from kivy.config import Config
-from kivy.core.window import Window
 import csv
 import japanize_kivy
-from kivy.properties import ListProperty
-from kivy.uix.relativelayout import RelativeLayout
-
-Config.set('graphics', 'width', 1920)
-Config.set('graphics', 'height', 1080)
+import os
 
 class DraggableButton(Button):
     def on_touch_down(self, touch):
@@ -34,12 +28,14 @@ class DraggableButton(Button):
         return super(DraggableButton, self).on_touch_move(touch)
 
 class MainApp(App):
-    background_color = ListProperty([1, 1, 1, 1])
-
     def build(self):
-        layout = GridLayout(cols=1, spacing=10, padding=10)
-        self.layout = layout
+        layout = GridLayout(cols=2, spacing=10, padding=10)
 
+        # 左側に機能ボタンを配置
+        left_layout = GridLayout(cols=1, spacing=10, size_hint_x=None, width=200)
+        right_layout = GridLayout(cols=1, spacing=10)
+        
+        # タイトルのラベル
         title_label = Label(
             text="ほしい機能を選んでね",
             font_size=24,
@@ -48,10 +44,6 @@ class MainApp(App):
             halign="center",
         )
 
-        with layout.canvas.before:
-            Color(*self.background_color)
-            self.background = Rectangle(pos=layout.pos, size=layout.size)
-        
         button1 = DraggableButton(text="時間表示設定", size_hint=(None, None))
         button1.bind(on_press=self.launch_main2)
 
@@ -64,102 +56,124 @@ class MainApp(App):
         button4 = DraggableButton(text="背景設定", size_hint=(None, None))
         button4.bind(on_press=self.launch_main5)
 
-        button5 = DraggableButton(text="確認画面", size_hint=(None, None))
-        button5.bind(on_press=self.launch_main6)
-
-        layout.add_widget(Label())
-        layout.add_widget(title_label)
-        layout.add_widget(button1)
-        layout.add_widget(button2)
-        layout.add_widget(button3)
-        layout.add_widget(button4)
-        layout.add_widget(button5)
-
+        # 確定ボタンを右側の上部に配置
         confirm_button = Button(text="確定", size_hint=(None, None))
         confirm_button.bind(on_press=self.save_button_positions)
-        layout.add_widget(confirm_button)
 
-        Window.bind(on_resize=self.update_background_size)
+        left_layout.add_widget(Label())  # 上部の余白用
+        left_layout.add_widget(title_label)
+        left_layout.add_widget(button1)
+        left_layout.add_widget(button2)
+        left_layout.add_widget(button3)
+        left_layout.add_widget(button4)
 
-        # 起動時にボタン位置を読み込み
-        self.load_button_positions()
+        layout.add_widget(left_layout)
+        layout.add_widget(right_layout)
+        layout.add_widget(confirm_button)  # 確定ボタンを追加
+
+        # 背景色のRGBA値をCSVから読み込み
+        background_color, _, _ = self.get_colors_from_csv("MAINSYS/CSV/color_settings.csv")
+
+        # 背景の色を設定
+        with layout.canvas.before:
+            self.background_color = Color(*background_color)  # 背景色をRGBAで設定
+            self.background_rect = Rectangle(pos=layout.pos, size=layout.size)
+
+        layout.bind(pos=self.update_background, size=self.update_background)
+
+        # 背景画像を設定
+        self.set_background_image_from_csv()
+
+        # ボタンの位置情報を保存するリスト
+        self.button_positions = []
 
         return layout
 
     def launch_main2(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
+        os.system("python button_clock.py")
 
     def launch_main3(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
+        os.system("python weather2.py")
+        os.system("python weathersearch.py")
 
     def launch_main4(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
+        os.system("python Calendar.py")
 
     def launch_main5(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
-
-    def launch_main6(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
-
-    def save_button_positions(self, instance):
-        button_positions = []
-        for child in self.layout.children:
-            if isinstance(child, DraggableButton):
-                button_positions.append([child.text, child.center_x, child.center_y])
-
-        with open("test/MAINSYS/CSV/dbutton_positions.csv", "w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerows(button_positions)
-
-    def load_button_positions(self):
-        button_positions = []
-        try:
-            with open("MAINSYS/CSV/button_positions.csv", "r", newline="") as file:
-                reader = csv.reader(file)
-                for row in reader:
-                    button_positions.append(row)
-        except FileNotFoundError:
-            pass
-
-        for text, x, y in button_positions:
-            for child in self.layout.children:
-                if isinstance(child, DraggableButton) and child.text == text:
-                    child.center_x = float(x)
-                    child.center_y = float(y)
+        os.system("python haikei.py")
 
     def on_start(self):
         background_color, title_color, subtitle_color = self.get_colors_from_csv("MAINSYS/CSV/color_settings.csv")
         self.set_background_color(background_color)
-
-        image_link = self.get_image_link_from_csv("MAINSYS/CSV/selected_backgrounds.csv")
-        if image_link:
-            self.set_background_image(image_link)
+        self.set_text_color(title_color, subtitle_color)
 
     def get_colors_from_csv(self, csv_file):
-        background_color = (1, 1, 1)
-        title_color = (0, 0, 0)
-        subtitle_color = (0, 0, 0)
+        background_color = (0.5, 0.7, 1, 1)  # 背景色（RGBA値を使用）
+        title_color = (0.1, 0.2, 0.3, 1)  # タイトル文字色（RGBA値を使用）
+        subtitle_color = (0.3, 0.4, 0.5, 1)  # サブタイトル文字色（RGBA値を使用）
 
         with open(csv_file, "r") as file:
             reader = csv.reader(file)
+            next(reader)  # ヘッダ行をスキップ
             for row in reader:
                 try:
-                    background_color = [float(val) for val in row]
-                    break
+                    background_color = (float(row[0]), float(row[1]), float(row[2]), float(row[3]))  # 背景色のRGBA値を設定
+                    if len(row) > 5:  # CSVファイルにタイトルとサブタイトルの色情報が含まれているか確認
+                        title_color = (float(row[4]), float(row[5]), float(row[6]), float(row[7]))  # タイトル文字色のRGBA値を設定
+                    if len(row) > 8:  # CSVファイルにサブタイトルの色情報が含まれているか確認
+                        subtitle_color = (float(row[4]), float(row[5]), float(row[6]), float(row[7]))  # サブタイトル文字色のRGBA値を設定
+                    break  # 最初の行の値を使用
                 except ValueError:
                     pass
-        return background_color
+        return background_color, title_color, subtitle_color
+
+    def update_background(self, instance, value):
+        self.background_rect.pos = instance.pos
+        self.background_rect.size = instance.size
 
     def set_background_color(self, color):
-        self.background_color = color
+        self.background_color.rgba = color
 
-    def update_background_size(self, instance, width, height):
-        self.background.size = (width, height)
+    def set_text_color(self, title_color, subtitle_color):
+        for child in self.root.children:
+            if isinstance(child, Label):
+                child.color = title_color
+
+    def set_background_image_from_csv(self):
+        # CSVファイルから背景画像のパスを取得
+        background_image_path = self.get_background_image_path("MAINSYS/CSV/selected_backgrounds.csv")
+        if background_image_path:
+            self.set_background_image(background_image_path)
+
+    def get_background_image_path(self, csv_file):
+        # 背景画像のパスをCSVファイルから取得
+        background_image_path = None
+        try:
+            with open(csv_file, "r", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if len(row) > 0:
+                        background_image_path = row[0]
+                        break  # 最初の行の値を使用
+        except FileNotFoundError:
+            pass
+        return background_image_path
+
+    def set_background_image(self, image_path):
+        # 背景画像のパスを指定して背景を設定
+        self.background_rect.source = image_path
+
+    def save_button_positions(self, instance):
+        # ボタンの位置情報を CSV ファイルに保存
+        with open("MAINSYS/CSV/button_positions.csv", "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["ButtonName", "X", "Y"])  # ヘッダー行
+
+            # ボタンの位置情報を保存
+            for child in self.root.children:
+                if isinstance(child, DraggableButton) and child.text != "確定":
+                    writer.writerow([child.text, child.center_x, child.center_y])
 
 if __name__ == "__main__":
-    MainApp().run()
+    main_app = MainApp()
+    main_app.run()
--- MAINSYS/teshaikei2.py
+++ MAINSYS/teshaikei2.py
@@ -1,77 +1,41 @@
-import os
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.graphics import Color, Rectangle
-from kivy.config import Config
-from kivy.core.window import Window
 import csv
 import japanize_kivy
-from kivy.properties import ListProperty
-from kivy.uix.relativelayout import RelativeLayout
-
-Config.set('graphics', 'width', 1920)
-Config.set('graphics', 'height', 1080)
-
-class DraggableButton(Button):
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            touch.grab(self)
-            return True
-        return super(DraggableButton, self).on_touch_down(touch)
-
-    def on_touch_up(self, touch):
-        if touch.grab_current is self:
-            touch.ungrab(self)
-            return True
-        return super(DraggableButton, self).on_touch_up(touch)
-
-    def on_touch_move(self, touch):
-        if touch.grab_current is self:
-            self.center_x = touch.x
-            self.center_y = touch.y
-            return True
-        return super(DraggableButton, self).on_touch_move(touch)
+import os
 
 class MainApp(App):
-    background_color = ListProperty([1, 1, 1, 1])
-    title_color = ListProperty([0, 0, 0, 1])
-    subtitle_color = ListProperty([0, 0, 0, 1])
-
     def build(self):
         layout = GridLayout(cols=1, spacing=10, padding=10)
-        self.layout = layout
 
+        # タイトルのラベル
         title_label = Label(
             text="ほしい機能を選んでね",
             font_size=24,
             size_hint_y=None,
             height=50,
             halign="center",
-            color=self.title_color  # タイトルの色を設定
         )
 
-        with layout.canvas.before:
-            Color(*self.background_color)  # 背景色を設定
-            self.background = Rectangle(pos=layout.pos, size=layout.size)
-        
-        button1 = DraggableButton(text="時間表示設定", size_hint=(None, None))
+        button1 = Button(text="時間表示設定", size_hint=(None, None))
         button1.bind(on_press=self.launch_main2)
 
-        button2 = DraggableButton(text="天気予報", size_hint=(None, None))
+        button2 = Button(text="天気予報", size_hint=(None, None))
         button2.bind(on_press=self.launch_main3)
 
-        button3 = DraggableButton(text="予定表示", size_hint=(None, None))
+        button3 = Button(text="予定表示", size_hint=(None, None))
         button3.bind(on_press=self.launch_main4)
 
-        button4 = DraggableButton(text="背景設定", size_hint=(None, None))
+        button4 = Button(text="背景設定", size_hint=(None, None))
         button4.bind(on_press=self.launch_main5)
 
-        button5 = DraggableButton(text="確認画面", size_hint=(None, None))
+        button5 = Button(text="確定", size_hint=(None, None))
         button5.bind(on_press=self.launch_main6)
 
-        layout.add_widget(Label())
+        layout.add_widget(Label())  # 上部の余白用
         layout.add_widget(title_label)
         layout.add_widget(button1)
         layout.add_widget(button2)
@@ -79,97 +43,103 @@ class MainApp(App):
         layout.add_widget(button4)
         layout.add_widget(button5)
 
-        confirm_button = Button(text="確定", size_hint=(None, None))
-        confirm_button.bind(on_press=self.save_button_positions)
-        layout.add_widget(confirm_button)
+        # 背景の色をCSVファイルから読み込んで設定
+        self.bg_color = Color(0, 0, 0, 1)  # デフォルトの背景色（RGBA形式で黒）
+        self.bg_rect = Rectangle(pos=layout.pos, size=layout.size)
 
-        Window.bind(on_resize=self.update_background_size)
+        layout.canvas.before.add(self.bg_color)
+        layout.canvas.before.add(self.bg_rect)
 
-        # 起動時にボタン位置を読み込み
-        self.load_button_positions()
+        # ウィンドウサイズ変更時に背景サイズを調整
+        layout.bind(size=self.adjust_background_size)
+
+        # 背景画像を設定
+        self.set_background_image()
 
         return layout
 
+    def adjust_background_size(self, instance, value):
+        # 背景のサイズをウィンドウサイズに合わせて調整
+        self.bg_rect.pos = instance.pos
+        self.bg_rect.size = instance.size
+
     def launch_main2(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
+        # main2.pyを実行
+        os.system("python button_clock.py")
 
     def launch_main3(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
+        # main3.pyを実行
+        os.system("python weather2.py")
+        os.system("python weathersearch.py")
 
     def launch_main4(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
+        # main3.pyを実行
+        os.system("python Calendar.py")
 
     def launch_main5(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
+        # main3.pyを実行
+        os.system("python haikei.py")
 
     def launch_main6(self, instance):
-        # この部分は適切なコードで置き換えてください
-        pass
-
-    def save_button_positions(self, instance):
-        button_positions = []
-        for child in self.layout.children:
-            if isinstance(child, DraggableButton):
-                button_positions.append([child.text, child.center_x, child.center_y])
-
-        with open("test/MAINSYS/CSV/dbutton_positions.csv", "w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerows(button_positions)
-
-    def load_button_positions(self):
-        button_positions = []
-        try:
-            with open("MAINSYS\CSV\color_settings.csv", "r", newline="") as file:
-                reader = csv.reader(file)
-                for row in reader:
-                    button_positions.append(row)
-        except FileNotFoundError:
-            pass
-
-        for text, x, y in button_positions:
-            for child in self.layout.children:
-                if isinstance(child, DraggableButton) and child.text == text:
-                    child.center_x = float(x)
-                    child.center_y = float(y)
+        # main3.pyを実行
+        os.system("python MAINSYS/teshaikei.py")
 
     def on_start(self):
-        self.load_colors_from_csv("MAINSYS\CSV\color_settings.csv")
-
-    def load_colors_from_csv(self, csv_file):
-        background_color, title_color, subtitle_color = self.get_colors_from_csv(csv_file)
+        # CSVファイルから背景色と文字の色を取得
+        background_color, title_color, subtitle_color = self.get_colors_from_csv("MAINSYS\CSV\color_settings.csv")
         self.set_background_color(background_color)
-        self.set_text_colors(title_color, subtitle_color)
+        self.set_text_color(title_color, subtitle_color)
+        print("背景色:", background_color)
 
     def get_colors_from_csv(self, csv_file):
-        background_color = [1, 1, 1, 1]
-        title_color = [0, 0, 0, 1]
-        subtitle_color = [0, 0, 0, 1]
+        background_color = (0, 0, 0, 1)  # デフォルトの背景色（RGBA形式で黒）
+        title_color = (1, 1, 1, 1)  # デフォルトのタイトル文字色（RGBA形式で白）
+        subtitle_color = (1, 1, 1, 1)  # デフォルトのサブタイトル文字色（RGBA形式で白）
 
-        with open(csv_file, "r") as file:
+        with open(csv_file, "r", encoding="utf-8") as file:
             reader = csv.reader(file)
+            # 1行目をスキップ
+            next(reader)
             for row in reader:
                 try:
-                    background_color = [float(val) for val in row[0:4]]
-                    title_color = [float(val) for val in row[4:8]]
-                    subtitle_color = [float(val) for val in row[8:12]]
-                    break
+                    background_color = (float(row[0]), float(row[1]), float(row[2]), float(row[3]))
+                    if len(row) > 4:  # CSVファイルにタイトルの色情報が含まれているか確認
+                        title_color = (float(row[4]), float(row[5]), float(row[6]), float(row[7]))
+                    if len(row) > 8:  # CSVファイルにサブタイトルの色情報が含まれているか確認
+                        subtitle_color = (float(row[8]), float(row[9]), float(row[10]), float(row[11]))
+                    break  # 最初の行の値を使用
                 except ValueError:
                     pass
         return background_color, title_color, subtitle_color
 
     def set_background_color(self, color):
-        self.background_color = color
+        self.bg_color.rgba = color
 
-    def set_text_colors(self, title_color, subtitle_color):
-        self.title_color = title_color
-        self.subtitle_color = subtitle_color
+    def set_text_color(self, title_color, subtitle_color):
+        # タイトルとサブタイトルの文字色を変更
+        for widget in self.root.walk(restrict=True):
+            if isinstance(widget, Label):
+                widget.color = title_color
 
-    def update_background_size(self, instance, width, height):
-        self.background.size = (width, height)
+    def set_background_image(self):
+        # 背景画像のパスを取得
+        background_image_path = self.get_background_image_path("MAINSYS/CSV/selected_backgrounds.csv")
+        if background_image_path:
+            self.bg_rect.source = background_image_path
+
+    def get_background_image_path(self, csv_file):
+        # 背景画像のパスをCSVファイルから取得
+        background_image_path = None
+        try:
+            with open(csv_file, "r", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if len(row) > 0:
+                        background_image_path = row[0]
+                        break  # 最初の行の値を使用
+        except FileNotFoundError:
+            pass
+        return background_image_path
 
 if __name__ == "__main__":
     MainApp().run()
