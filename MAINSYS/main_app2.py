d8cf5afa61d0ca29212884726854466664560181
gotohoroya <204067@ocsjoho.onmicrosoft.com>
2023-11-20 16:12:54 +0900

    0000

--- MAINSYS/main_app2.py
+++ MAINSYS/main_app2.py
@@ -1,6 +1,19 @@
-# main_app2.py
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from kivy.lang import Builder
+from syoki import MainApp
+from kivy.factory import Factory
 
-from main_app import WeatherApp
+Factory.register('MainApp', cls=MainApp)
+kv = '''
+BoxLayout:
+    orientation: 'horizontal'
+    MainApp:
+'''
+
+class ExternalApp(App):
+    def build(self):
+        return Builder.load_string(kv)
 
 if __name__ == '__main__':
-    WeatherApp().run()
+    ExternalApp().run()
