665ef73197cea2e521ec1259179b3ced32b0f909
ishikawa <204004@ocsjoho.onmicrosoft.com>
2023-11-28 09:39:55 +0900

    2023/11/28

--- MAINSYS/onoD_1day_weather.py
+++ MAINSYS/onoD_1day_weather.py
@@ -8,7 +8,6 @@ from kivy.clock import Clock
 import requests
 import japanize_kivy
 import csv
-import os
 
 class WeatherApp(App):
     def load_csv(self):
@@ -16,7 +15,6 @@ class WeatherApp(App):
         with open(file_path, mode='r') as file:
             reader = csv.reader(file)
             data = list(reader)
-
         url_data = data[2][1]
 
         with open(file_path, mode='w', newline='') as file:
@@ -29,18 +27,24 @@ class WeatherApp(App):
         self.api_url = "https://api.open-meteo.com/v1/forecast?latitude=34.7&longitude=135.5&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=Asia%2FTokyo"
         self.data = None
 
-        self.weather_layout = BoxLayout(orientation='vertical')
+        # ルート レイアウトを作成
+        self.root_layout = BoxLayout(orientation='vertical')
 
+        # 天気情報のための BoxLayout を作成
+        self.weather_layout = BoxLayout()
+        self.root_layout.add_widget(self.weather_layout)
+
+        # 初回のデータ取得と定期的な更新をスケジュール
         self.update_weather_data()
-        Clock.schedule_interval(lambda dt: self.update_weather_data(), 3600)
+        Clock.schedule_interval(lambda dt: self.update_weather_data(), 3600)  # 1時間ごとに更新
 
-        return self.weather_layout
+        return self.root_layout
 
     def update_weather_data(self, dt=None):
         response = requests.get(self.api_url)
         data = response.json()
         self.data = data
-        self.update_display(1)
+        self.update_display(1)  # 初回は1日分のデータを表示
 
     def update_display(self, days):
         if self.data:
@@ -51,8 +55,8 @@ class WeatherApp(App):
             weather_code = daily_data.get("weather_code", [])
 
             if time and max_temperature and min_temperature and weather_code:
-                weekly_data = []
-                for i in range(min(days, len(time))):
+                weekly_data = []  # 指定された日数分のデータを格納するリスト
+                for i in range(min(days, len(time))):  # days とデータの個数の小さい方を使用
                     day_data = {
                         'day': time[i],
                         'max_temp': max_temperature[i],
@@ -61,9 +65,11 @@ class WeatherApp(App):
                     }
                     weekly_data.append(day_data)
 
+                # 指定された日数分のデータを更新
                 self.update_weekly_data(weekly_data)
 
     def update_weekly_data(self, weekly_data):
+        # 指定された日数分のデータをレイアウトに反映
         self.weather_layout.clear_widgets()
         for day_data in weekly_data:
             day_layout = BoxLayout(orientation='vertical', spacing=5)
@@ -74,6 +80,7 @@ class WeatherApp(App):
             min_temp_label = Label(text=f"最低気温: {day_data['min_temp']}°C", font_size='20sp')
             weather_label = Label(text=f"天気: {day_data['weather']}", font_size='20sp')
             dLabel2 = Label()
+            # 天気に対応する画像を表示
             weather_image = Image(source=get_weather_image(day_data['weather']))
             day_layout.add_widget(title_label)
             day_layout.add_widget(dLabel1)
@@ -112,9 +119,8 @@ def get_weather_meaning(weather_code):
         return "不明"
 
 def get_weather_image(weather_meaning):
-    # 仮の実装: 天気に応じて異なる画像を返す
     if '晴れ' in weather_meaning:
-        return 'test/onoD/sun.png' #実施環境用にパスを変更してください
+        return 'test/onoD/sun.png'  # 実施環境用にパスを変更してください
     elif '雨' in weather_meaning:
         return 'test/onoD/umbrella.png'
     elif '霞、ほこり、砂または煙' in weather_meaning:
